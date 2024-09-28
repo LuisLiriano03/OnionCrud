8c606df1a9128b5c2aabd3aa13a9a2ef58ca52a7
LuisLiriano03 <llirianomendez@hotmail.com>
2024-09-29 01:14:10 +0200

    update and create

---
+++ OnionCrud.API/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionCrud.API.Utility;
+using OnionCrud.Application.Authentication.DTOs;
+using OnionCrud.Application.Authentication.Interfaces;
+using OnionCrud.Application.Users.DTOs;
+
+namespace OnionCrud.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> login([FromBody] LoginRequest login)
+        {
+            var response = new Response<LoginResponse>();
+
+            try
+            {
+                response.status = true;
+                response.value = await _authService.Login(login.Email, login.PasswordHash);
+                response.message = "User logged in successfully";
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.message = ex.Message;
+            }
+
+            return Ok(response);
+
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> register([FromBody] CreateUser user)
+        {
+            var response = new Response<GetUser>();
+
+            try
+            {
+                response.status = true;
+                response.value = await _authService.Register(user);
+                response.message = "Registration successful";
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.message = ex.Message;
+            }
+
+            return Ok(response);
+
+        }
+
+
+
+    }
+}
--- OnionCrud.API/Controllers/UserController.cs
+++ OnionCrud.API/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OnionCrud.API.Utility;
+using OnionCrud.Application.Users.DTOs;
+using OnionCrud.Application.Users.Interfaces;
 
 namespace OnionCrud.API.Controllers
 {
@@ -7,5 +11,81 @@ namespace OnionCrud.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("getallusers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var response = new Response<List<GetUser>>();
+
+            try
+            {
+                response.status = true;
+                response.value = await _userService.GetAllUserAsync();
+                response.message = "Successful data";
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.message = ex.Message;
+            }
+
+            return Ok(response);
+
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> EditUser([FromBody] UpdateUser user)
+        {
+            var response = new Response<bool>();
+
+            try
+            {
+                response.status = true;
+                response.value = await _userService.UpdateAsync(user);
+                response.message = "User information updated successfully";
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.message = ex.Message;
+            }
+
+            return Ok(response);
+
+        }
+
+        [Authorize]
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> SoftDeleteUser(int id)
+        {
+            var response = new Response<bool>();
+
+            try
+            {
+                response.value = await _userService.SoftDeleteAsync(id);
+                response.status = true;
+                response.message = "User soft deleted successfully";
+            }
+            catch (Exception ex)
+            {
+                response.status = false;
+                response.message = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
     }
+
 }
