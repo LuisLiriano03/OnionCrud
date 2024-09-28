70efb3441bc75e5b27e3753feb639b9439fceb9e
LuisLiriano03 <llirianomendez@hotmail.com>
2024-09-29 01:12:47 +0200

    added response

---
+++ OnionCrud.API/Utility/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace OnionCrud.API.Utility
+{
+    public class Response<T>
+    {
+        public bool status { get; set; }
+        public T value { get; set; }
+        public string message { get; set; }
+    }
+}
