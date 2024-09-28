9a5c0a53b8b9e913ddc8a593ede12c84073858df
LuisLiriano03 <llirianomendez@hotmail.com>
2024-09-29 01:19:26 +0200

    IoC added

---
+++ OnionCrud.Application/IoC.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionCrud.Application.Authentication.Interfaces;
+using OnionCrud.Application.Authentication.Services;
+using OnionCrud.Application.Users.Interfaces;
+using OnionCrud.Application.Users.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrud.Application
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection service)
+        {
+            return service
+                .AddScoped<IAuthService, AuthService>()
+                .AddScoped<IUserService, UserService>();
+
+
+
+        }
+
+    }
+}
